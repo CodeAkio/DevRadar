883365d6187599d0a944b746f502d08ea4e25557
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-01-17 17:58:49 -0300

    Real time

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -6,11 +6,16 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import api from '../services/api'
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket'
 
 function Main({ navigation }) {
     const [devs, setDevs] = useState([])
     const [currentRegion, setCurrentRegion] = useState(null)
     const [techs, setTechs] = useState('')
+
+    useEffect(() => {
+        subscribeToNewDevs(dev => setDevs([...devs, dev]))
+    }, [devs])
 
     useEffect(() => {
         async function loadInitialPosition() {
@@ -35,6 +40,18 @@ function Main({ navigation }) {
         loadInitialPosition()
     }, [])
 
+    function setupWebsocket() {
+        disconnect()
+
+        const { latitude, longitude } = currentRegion
+
+        connect(
+            latitude,
+            longitude,
+            techs,
+        )
+    }
+
     async function loadDevs() {
         const { latitude, longitude } = currentRegion
 
@@ -47,6 +64,7 @@ function Main({ navigation }) {
         })
 
         setDevs(response.data.devs)
+        setupWebsocket()
     }
 
     function handleRegionChanged(region) {
